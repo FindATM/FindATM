9df204fa4ded9646a14e6337db0b1e4c12dc2735
Harris Spentzas <spentzas@Harriss-iMac.local>
2015-07-03 22:23:14 +0300

    Initial Commit

---
+++ AtmMoney/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AtmMoney
+//
+//  Created by Harris Spentzas on 7/3/15.
+//  Copyright (c) 2015 Harris Spentzas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
