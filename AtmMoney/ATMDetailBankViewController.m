99715247cac2cfe2d1ce8c342e59a2deecdaa6af
Dimitris C <dimmdesign@DimsMacBook-Pro.local>
2015-07-05 14:03:13 +0300

    many changes

--- AtmMoney/ATMDetailBankViewController.m
+++ AtmMoney/ATMDetailBankViewController.m
@@ -24,6 +24,7 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *latestActivityLabel;
 @property (weak, nonatomic) IBOutlet UITableView *latestActivityTableView;
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
 
 @end
 
@@ -38,20 +39,73 @@ static NSString *activityCellItemIdentifier = @"activityCellItemIdentifier";
     
     self.view.backgroundColor = [UIColor whiteColor];
     
+    [self.submitButton addTarget:self action:@selector(submitButtonPressed) forControlEvents:UIControlEventTouchUpInside];
     
-    self.hasMoneyLabel.text = NSLocalizedStringFromTable(@"detail.view.hasmoney", @"Localization", @"");
-    self.hasTwentiesLabel.text = NSLocalizedStringFromTable(@"detail.view.hastwenties", @"Localization", @"");
-    
+    // Activity Table View init
     self.latestActivityTableView.delegate = self;
     self.latestActivityTableView.dataSource = self;
 
     [self.latestActivityTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:activityCellItemIdentifier];
+    
+    //
+    self.hasMoneyLabel.text = NSLocalizedStringFromTable(@"detail.view.hasmoney", @"Localization", @"");
+    self.hasTwentiesLabel.text = NSLocalizedStringFromTable(@"detail.view.hastwenties", @"Localization", @"");
+    
+    [self.hasMoneySwitch addTarget:self action:@selector(hasMoneySwitchValueChanged) forControlEvents:UIControlEventValueChanged];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Submit Button -
+- (void)submitButtonPressed {
+    
+}
+
+#pragma mark - Has Money & Has Twenties methods -
+
+- (void)hasMoneySwitchValueChanged {
+ 
+    if (self.hasMoneySwitch.isOn)
+        [self enableTwentiesView];
+    else
+        [self disableTwentiesView];
+    
+}
+
+- (void)enableTwentiesView {
+    
+    self.hasTwentiesView.userInteractionEnabled = YES;
+    [self.hasTwentiesSwitch addTarget:self action:@selector(hasTwentiesSwitchValueChanged) forControlEvents:UIControlEventValueChanged];
+
+    [UIView animateWithDuration:0.35 animations:^{
+        self.hasTwentiesView.alpha = 1.0f;
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+- (void)disableTwentiesView {
+    
+    self.hasTwentiesView.userInteractionEnabled = NO;
+    [self.hasTwentiesSwitch setOn:NO animated:YES];
+    [self.hasTwentiesSwitch removeTarget:self action:@selector(hasTwentiesSwitchValueChanged) forControlEvents:UIControlEventValueChanged];
+    [UIView animateWithDuration:0.35 animations:^{
+        self.hasTwentiesView.alpha = 0.3f;
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+
+- (void)hasTwentiesSwitchValueChanged {
+    
+}
+
+#pragma mark - Table View Methods -
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning to be changed to show the latest activity.
@@ -61,7 +115,9 @@ static NSString *activityCellItemIdentifier = @"activityCellItemIdentifier";
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:activityCellItemIdentifier forIndexPath:indexPath];
-    
+    cell.backgroundColor = (indexPath.row % 2 == 0) ? [UIColor colorWithRed:0.97 green:0.97 blue:0.97 alpha:1.0] : [UIColor whiteColor];
+    cell.selectedBackgroundView = [[UIView alloc] init];
+
     return cell;
 }
 
